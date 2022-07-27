e090ac8fd48303ea5c2602e3a83bebce8424755e
Vincent Naples <dreambaitrecordings@gmail.com>
2022-07-26 20:29:19 -0500

    20

--- logs/2022.07.26.py
+++ logs/2022.07.26.py
@@ -47,3 +47,25 @@
 
 2022.07.26 13:57:28 [DEBUG] msg: KEYMACROS	| onShortcuts: <F5>	| Toggle cook_bar probe overlay
 
+2022.07.26 20:14:06 [INFO] msg: SceneChanger | SwitchToScene(index=0, fadeTime=0): 'SPOUT'
+2022.07.26 20:14:06 [INFO] msg: SceneChanger | SwitchToScene(index=1, fadeTime=0): 'TEST_PATTERNS'
+2022.07.26 20:14:06 [INFO] msg: SceneChanger | SwitchToScene(index=2, fadeTime=0): 'SceneTemplate'
+2022.07.26 20:14:06 [INFO] msg: SceneChanger | SwitchToScene(index=3, fadeTime=0): 'equirectangularRemap'
+2022.07.26 20:14:06 [DEBUG] msg: onInit | Scene: equirectangularRemap
+2022.07.26 20:14:15 [DEBUG] msg: onInit | Scene: SPOUT
+2022.07.26 20:14:15 [DEBUG] msg: onStart | Scene: SPOUT
+2022.07.26 20:14:15 [DEBUG] msg: onExit | Scene: SPOUT
+2022.07.26 20:14:15 [DEBUG] msg: onInit | Scene: SceneTemplate
+2022.07.26 20:14:16 [DEBUG] msg: onStart | Scene: SceneTemplate
+2022.07.26 20:14:16 [DEBUG] msg: onExit | Scene: SceneTemplate
+2022.07.26 20:14:16 [DEBUG] msg: onInit | Scene: TEST_PATTERNS
+2022.07.26 20:14:16 [DEBUG] msg: onStart | Scene: TEST_PATTERNS
+2022.07.26 20:14:16 [DEBUG] msg: onExit | Scene: TEST_PATTERNS
+2022.07.26 20:14:16 [DEBUG] msg: onStart | Scene: equirectangularRemap
+2022.07.26 20:14:22 [DEBUG] msg: SceneExt	| Initdatatable()	'/project1/sceneLib/SPOUT'
+2022.07.26 20:14:22 [DEBUG] msg: SceneExt	| Initdatatable()	'/project1/sceneLib/SceneTemplate'
+2022.07.26 20:14:22 [DEBUG] msg: SceneExt	| Initdatatable()	'/project1/sceneLib/TEST_PATTERNS'
+2022.07.26 20:16:38 [INFO] msg: SceneChanger | SwitchToScene(index=1, fadeTime=0): 'TEST_PATTERNS'
+2022.07.26 20:16:38 [DEBUG] msg: onInit | Scene: equirectangularRemap
+2022.07.26 20:20:32 [DEBUG] msg: KEYMACROS	| onShortcuts: <F5>	| Toggle cook_bar probe overlay
+
