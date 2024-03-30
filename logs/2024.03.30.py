35c1ad1c334a01019dbea24fbf9b61251510c4a6
Vincent Naples <dreambaitrecordings@gmail.com>
2024-03-30 16:03:42 -0700

    update dome 3j

---
+++ logs/2024.03.30.py
@@ -0,0 +1,42 @@
+2024.03.30 15:52:26 [INFO] msg: SceneChanger | SwitchToScene(index=0, fadeTime=0): 'SPOUT'
+2024.03.30 15:52:26 [INFO] msg: SceneChanger | SwitchToScene(index=1, fadeTime=0): 'TEST_PATTERNS'
+2024.03.30 15:52:26 [INFO] msg: SceneChanger | SwitchToScene(index=2, fadeTime=0): 'SceneTemplate'
+2024.03.30 15:52:26 [DEBUG] msg: onInit | Scene: equirectangularRemap
+2024.03.30 15:52:31 [INFO] msg: SceneChanger | SwitchToScene(index=3, fadeTime=0): 'equirectangularRemap'
+2024.03.30 15:52:32 [DEBUG] msg: onInit | Scene: SPOUT
+2024.03.30 15:52:32 [DEBUG] msg: onStart | Scene: SPOUT
+2024.03.30 15:52:32 [DEBUG] msg: onExit | Scene: SPOUT
+2024.03.30 15:52:32 [DEBUG] msg: onInit | Scene: SceneTemplate
+2024.03.30 15:52:32 [DEBUG] msg: onStart | Scene: SceneTemplate
+2024.03.30 15:52:32 [DEBUG] msg: onExit | Scene: SceneTemplate
+2024.03.30 15:52:32 [DEBUG] msg: onInit | Scene: TEST_PATTERNS
+2024.03.30 15:52:32 [DEBUG] msg: onStart | Scene: TEST_PATTERNS
+2024.03.30 15:52:32 [DEBUG] msg: onExit | Scene: TEST_PATTERNS
+2024.03.30 15:52:32 [DEBUG] msg: onInit | Scene: equirectangularRemap
+2024.03.30 15:52:32 [DEBUG] msg: onStart | Scene: equirectangularRemap
+2024.03.30 15:52:33 [DEBUG] msg: onStart | Scene: equirectangularRemap
+2024.03.30 15:52:33 [DEBUG] msg: onInit | Scene: SceneTemplate
+2024.03.30 15:52:35 [DEBUG] msg: SceneExt	| Initdatatable()	'/project1/sceneLib/SPOUT'
+2024.03.30 15:52:35 [DEBUG] msg: SceneExt	| Initdatatable()	'/project1/sceneLib/SceneTemplate'
+2024.03.30 15:52:35 [DEBUG] msg: SceneExt	| Initdatatable()	'/project1/sceneLib/TEST_PATTERNS'
+2024.03.30 15:53:24 [DEBUG] msg: KEYMACROS	| onShortcuts: <shift.n>	| Call INSPECTORGADGET to instigate TWEENER methods to interpolateto the normMin value for ui rolloverPar
+
+2024.03.30 15:53:24 [DEBUG] msg: KEYMACROS	| onShortcuts: <shift.d>	| Call INSPECTORGADGET to instigate TWEENER methods to interpolateto the default value for ui rolloverPar
+
+2024.03.30 15:53:26 [DEBUG] msg: onInit | Scene: NDI
+2024.03.30 15:53:26 [INFO] msg: SCENES | New Scene: "/project1/sceneLib/NDI" successfully created!
+2024.03.30 15:53:56 [INFO] msg: SceneChanger | SwitchToScene(index=4, fadeTime=0): 'NDI'
+2024.03.30 15:53:56 [DEBUG] msg: onExit | Scene: equirectangularRemap
+2024.03.30 15:53:56 [DEBUG] msg: onStart | Scene: NDI
+2024.03.30 15:53:56 [DEBUG] msg: onInit | Scene: equirectangularRemap
+2024.03.30 15:53:58 [DEBUG] msg: SceneExt	| Initdatatable()	'/project1/sceneLib/equirectangularRemap'
+2024.03.30 15:54:42 [DEBUG] msg: KEYMACROS	| onShortcuts: <F5>	| Toggle cook_bar probe overlay
+
+2024.03.30 15:54:50 [DEBUG] msg: KEYMACROS	| onShortcuts: <F1>	| toggle perform mode
+
+2024.03.30 15:54:51 [DEBUG] msg: KEYMACROS	| onShortcuts: <F1>	| toggle perform mode
+
+2024.03.30 15:54:52 [DEBUG] msg: KEYMACROS	| onShortcuts: <F1>	| toggle perform mode
+
+2024.03.30 15:56:15 [DEBUG] msg: KEYMACROS	| onShortcuts: <F5>	| Toggle cook_bar probe overlay
+
